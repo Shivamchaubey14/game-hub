850e1adf743a64aea76628d872bb653899651e28
Shivam Chaubey <shivamc36@gmail.com>
2024-08-12 17:30:23 +0530

    Implemented searching functionality

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -34,7 +35,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
 
       <Show above="lg">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { px } from "framer-motion";
 import SearchInput from "./SearchInput";
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const NavBar = ({ onSearch }: Props) => {
   return (
     <div>
       <HStack padding={"20px"}>
         <Image src={logo} boxSize="60px" />
-        <SearchInput />
+        <SearchInput onSearch={onSearch} />
         <ColorModeSwitch />
       </HStack>
     </div>
