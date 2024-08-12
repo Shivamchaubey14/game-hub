68af14e6200ec37d48bc02ce02e8420cb863567d
Shivam Chaubey <shivamc36@gmail.com>
2024-08-12 19:24:15 +0530

    Gave the equal spacing to heading and genres and cleaned the code

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,18 @@
-import { HStack, List, ListItem, Image, Text, Button } from "@chakra-ui/react";
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Text,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null
+  selectedGenre: Genre | null;
 }
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
@@ -13,29 +21,35 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (isLoading) return <GenreListSkeleton />;
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={genre.image_background}
-              marginY={"4px"}
-              marginX={"4px"}
-            />
-            <Button
-              fontSize="lg"
-              variant="link"
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-            >
-              {genre.name.toUpperCase()}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading marginLeft="16px" marginBottom="8px">
+        Genres
+      </Heading>
+      <List spacing={3} marginX="16px">
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack spacing={4}>
+              <Image
+                objectFit={"cover"}
+                boxSize="32px"
+                borderRadius={8}
+                src={genre.image_background}
+              />
+              <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                fontSize="lg"
+                variant="link"
+                onClick={() => onSelectGenre(genre)}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              >
+                {genre.name.toUpperCase()}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
